08e5b5e86bf44423a110c49d734cf2cb2ccd7357
Amardeep80013 <amardeepsharma259@gmail.com>
2024-01-19 16:03:58 +0530

    Dajngo project incomplete openai does not response

---
+++ djangochatbot/chatbot/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import openai
+
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+def ask_openai(message):
+    response = openai.completions.create(
+        # model="text-davinci-003",
+       model='gpt-3.5-turbo-instruct',
+        prompt=message,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    # print(response)
+    answer = response.choice[0].text.strip()
+    return answer
+
+# Create your views here.
+def chatbot(request):
+  if request.method == 'POST':
+    message = request.POST.get('message')
+    response = ask_openai(message)
+    return JsonResponse({'message': message, 'response': response})
+
+  return render(request, 'chatbot.html')
